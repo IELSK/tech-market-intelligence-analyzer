b2c36f6d44f60994f38c459313f0429b6ff53b97
IELSK <pr.rodriguesb@outlook.com>
2026-03-16 14:42:02 -0300

    adding git ignore

--- pipeline/ingest_stackoverflow.py
+++ pipeline/ingest_stackoverflow.py
@@ -5,3 +5,7 @@ RAW_DIR = Path("data/raw")
 PROCESSED_DIR = Path("data/processed")
 PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
 
+CSV_FILE = RAW_DIR / "survey_results_public.csv"
+
+df = pd.read_csv(CSV_FILE, low_memory=False)
+print(f"Shape bruto: {df.shape}")
